278c37f5421b52301ddc2def5766ca566e5ed672
GlebKolyano <glebkolyano38@gmail.com>
2021-09-23 11:52:22 +0300

    added js code / finished

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,9 +1,37 @@
-function ibg() {
-  let ibg = document.querySelectorAll(".ibg");
-  for (var i = 0; i < ibg.length; i++) {
-    if (ibg[i].querySelector('img')) {
-      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+
+const container = document.querySelector('.container')
+
+let countSlides = mainSlide.querySelectorAll('div').length
+let indexSlide = 0
+
+sidebar.style.top = `-${(countSlides-1)*100}vh`
+
+upBtn.addEventListener('click', () => {
+  changeSlide('up')
+})
+downBtn.addEventListener('click', () => {
+  changeSlide('down')
+})
+
+function changeSlide(direction) {
+  if (direction === 'up') {
+    indexSlide++
+    if (indexSlide === countSlides) {
+      indexSlide = 0
     }
   }
-}
-ibg();
+  else if (direction === 'down') {
+    indexSlide--
+    if (indexSlide < 0) {
+      indexSlide = countSlides - 1
+    }
+  }
+  const heightV = container.clientHeight
+  mainSlide.style.transform = `translateY(-${indexSlide * heightV}px)`
+  sidebar.style.transform = `translateY(${indexSlide * heightV}px)`
+};
